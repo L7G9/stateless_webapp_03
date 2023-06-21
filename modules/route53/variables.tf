f5be5e985ba51b2bfe4489525f6990b64b6298dd
l7g9 <lukewgregory@gmail.com>
2023-06-21 09:39:10 +0100

    refactor(route53): add tag prefix variable

--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -1,3 +1,8 @@
+variable "name_tag_prefix" {
+  type        = string
+  description = "Prefix to give all name tags of resources created by this module"
+}
+
 variable "hosted_zone_name" {
   type        = string
   description = "Name of hosted zone to create alias record in"
