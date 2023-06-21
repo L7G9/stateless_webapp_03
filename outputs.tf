ed881967206b37e64d908ab026750e02a966a999
l7g9 <lukewgregory@gmail.com>
2023-06-21 09:41:46 +0100

    feat(main): add URL of load balancer alias record

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "lb_dns_name" {
   value       = module.load_balancer.lb_dns_name
   description = "DNS name assigned to load balancer"
 }
+
+output "route53_alias_name" {
+  value       = module.route53.alias_record_name
+  description = "URL of alias record poiting to load balancer"
+}
