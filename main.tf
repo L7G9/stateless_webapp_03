5e0b545550199075d17547e284b115404650bd30
l7g9 <lukewgregory@gmail.com>
2023-06-27 08:26:03 +0100

    feat(main): add route53 module

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ module "load_balancer" {
   security_groups = [module.security.lb_security_group_id]
   subnets         = module.network.public_subnet_ids
   vpc             = module.network.vpc_id
+  certificate_arn = "arn:aws:acm:eu-west-2:382801774683:certificate/643f4f71-e7d0-4e03-935f-ef6cbd8bdbd0"
 }
 
 module "auto_scaling_group" {
@@ -43,4 +44,13 @@ module "auto_scaling_group" {
   instance_security_groups = [module.security.instance_security_group_id]
   instance_subnets         = module.network.private_subnet_ids
   target_group_arns        = [module.load_balancer.lb_target_group_arn]
+}
+
+module "route53" {
+  source = "./modules/route53"
+
+  name_tag_prefix = var.name_tag_prefix
+  hosted_zone_name = var.domain_name
+  subdomain = var.subdomain
+  load_balancer_arn = module.load_balancer.lb_arn
 }
