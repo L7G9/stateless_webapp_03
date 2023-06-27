f6438c86b0fc82faeb58e83d9aa5fa04750842cd
l7g9 <lukewgregory@gmail.com>
2023-06-27 13:08:41 +0100

    refactor(auto_scaling_group): add variables to increase module options

--- modules/auto_scaling_group/main.tf
+++ modules/auto_scaling_group/main.tf
@@ -24,11 +24,11 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_launch_configuration" "main" {
-  name_prefix     = "swa-03-"
+  name_prefix     = "${var.name_tag_prefix}-asg-instance-"
   image_id        = data.aws_ami.amazon_linux.id
-  instance_type   = "t2.micro"
-  user_data       = file("./files/user-data.sh")
-  security_groups = var.instance_security_groups
+  instance_type   = var.instance_type
+  user_data       = file(var.user_data_file_path)
+  security_groups = var.instance_security_group_ids
 
   lifecycle {
     create_before_destroy = true
@@ -36,12 +36,18 @@ resource "aws_launch_configuration" "main" {
 }
 
 resource "aws_autoscaling_group" "main" {
-  min_size                  = 1
-  max_size                  = 3
-  desired_capacity          = 1
+  min_size                  = var.minimum_number_of_instances
+  max_size                  = var.maximum_number_of_instances
+  desired_capacity          = var.desired_number_of_instances
   health_check_type         = "ELB"
   health_check_grace_period = 600
   launch_configuration      = aws_launch_configuration.main.name
-  vpc_zone_identifier       = var.instance_subnets
+  vpc_zone_identifier       = var.instance_subnet_ids
   target_group_arns         = var.target_group_arns
+
+  tag {
+    key                 = "Name"
+    value               = "${var.name_tag_prefix}-main"
+    propagate_at_launch = true
+  }
 }
--- modules/auto_scaling_group/variables.tf
+++ modules/auto_scaling_group/variables.tf
@@ -3,17 +3,49 @@ variable "name_tag_prefix" {
   description = "Prefix to give all name tags of resources created by this module"
 }
 
-variable "instance_security_groups" {
-  type        = list(string)
-  description = "Security groups to add instances to"
+variable "instance_type" {
+  type        = string
+  description = "Instance type to launch"
+  default     = "t2.micro"
 }
 
-variable "instance_subnets" {
+variable "user_data_file_path" {
+  type        = string
+  description = "Script to run when launching the instance"
+  default     = "./files/user-data.sh"
+}
+
+variable "minimum_number_of_instances" {
+  type        = number
+  description = "Minimum size of the Auto Scaling Group"
+  default     = 1
+}
+
+variable "maximum_number_of_instances" {
+  type        = number
+  description = "Maximum size of the Auto Scaling Group"
+  default     = 3
+}
+
+variable "desired_number_of_instances" {
+  type        = number
+  description = "Number of instances that should be running in the Auto Scaling Group"
+  default     = 1
+}
+
+variable "instance_security_group_ids" {
   type        = list(string)
-  description = "Subnet IDs to launch instances in"
+  description = "Security group Ids to add instances to"
+}
+
+variable "instance_subnet_ids" {
+  type        = list(string)
+  description = "Subnet Ids to launch instances in"
 }
 
 variable "target_group_arns" {
   type        = list(string)
-  description = "Target groups to add instances to"
+  description = "Target group ARNs to add instances to"
 }
+
+
