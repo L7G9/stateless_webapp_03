77e237b72622eaed73a20bde1edd3796bfa4181c
l7g9 <lukewgregory@gmail.com>
2023-06-27 13:30:14 +0100

    style(main): update variable names used by load_balancer module

--- main.tf
+++ main.tf
@@ -30,11 +30,11 @@ module "security" {
 module "load_balancer" {
   source = "./modules/load_balancer"
 
-  name_tag_prefix = var.name_tag_prefix
-  security_groups = [module.security.lb_security_group_id]
-  subnets         = module.network.public_subnet_ids
-  vpc             = module.network.vpc_id
-  certificate_arn = "arn:aws:acm:eu-west-2:382801774683:certificate/643f4f71-e7d0-4e03-935f-ef6cbd8bdbd0"
+  name_tag_prefix    = var.name_tag_prefix
+  security_group_ids = [module.security.lb_security_group_id]
+  subnet_ids         = module.network.public_subnet_ids
+  vpc_id             = module.network.vpc_id
+  certificate_arn    = "arn:aws:acm:eu-west-2:382801774683:certificate/643f4f71-e7d0-4e03-935f-ef6cbd8bdbd0"
 }
 
 module "auto_scaling_group" {
